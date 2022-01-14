adea0d513a7f3f38d9a775abd1a968a671215241
Aloïs HIAS <aloishias@macbook-pro-de-alois.home>
2022-01-14 14:41:09 +0100

    Add service task and features getTaks for the dropdown of add-task compoment

--- src/app/add-task/add-task.component.ts
+++ src/app/add-task/add-task.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../entity/task';
+import { TaskService } from '../service/task.service';
 
 @Component({
   selector: 'app-add-task',
@@ -9,9 +11,18 @@ export class AddTaskComponent implements OnInit {
 
   pageName = "Ajout d'une tâche";
 
-  constructor() { }
+  task = <Task>{};
+  tasks: Task[] = [];
+
+  constructor(private taskService : TaskService) { }
 
   ngOnInit(): void {
+    this.getTasks();
+  }
+
+  getTasks(): void {
+    this.taskService.getTasks()
+    .subscribe(tasks => this.tasks = tasks);
   }
 
 }
