52eb501aefdc96d7d8e4a5c4e2703b9552b7f6e6
aloishias <hias.alois@gmail.com>
2022-01-25 11:38:44 +0100

    Add form to add weekly-report

--- src/app/add-weekly-report/add-weekly-report.component.ts
+++ src/app/add-weekly-report/add-weekly-report.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
+interface TaskState{
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-add-weekly-report',
   templateUrl: './add-weekly-report.component.html',
@@ -11,5 +21,17 @@ export class AddWeeklyReportComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
+
+  taskStates: TaskState[] = [
+    {value: 'inProgress-0', viewValue: 'En cours'},
+    {value: 'done-1', viewValue: 'Finit'},
+    {value: 'blocked-2', viewValue: 'Bloqué'},
+  ];
 
 }
